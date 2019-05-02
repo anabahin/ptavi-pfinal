da74ac2e1f28ca9b578d9f573bd9559670afdc72
anabahin <alinabahin3@gmail.com>
2019-05-02 19:36:44 +0200

    Update uaclient.py
    implementado mensaje REGISTER

--- uaclient.py
+++ uaclient.py
@@ -3,6 +3,8 @@
 
 import os
 import sys
+import socket
+from hashlib import md5
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
@@ -22,6 +24,40 @@ class XMLHandler(ContentHandler):
 
     def get_tags(self):
         return self.tags
+
+def digest_response(nonce, passwd, username):
+    digest = md5()
+    digest.update(bytes(nonce, 'utf-8'))
+    digest.update(bytes(passwd, 'utf-8'))
+    digest.update(bytes(username, 'utf-8'))
+    digest.digest()
+    return digest.hexdigest()
+
+def send_register(socket, option):
+    mess = 'REGISTER sip:' + config['account_username'] + ':'
+    mess += config['uaserver_puerto'] + ' SIP/2.0\r\nExpires: ' + str(option)
+    print('Enviado -- ', mess)
+    socket.send(bytes(mess, 'utf-8'))
+    
+def send_register_digest(socket, option, digest):
+    mess = 'REGISTER sip:' + config['account_username'] + ':'
+    mess += config['uaserver_puerto'] + ' SIP/2.0\r\nExpires: ' + str(option)
+    mess += '\r\nAuthorization: Digest response="'
+    mess += digest + '"\r\n'
+    print('Enviado -- ', mess)
+    socket.send(bytes(mess, 'utf-8'))
+
+def receive_message(socket):
+    pr_ip = proxy_address[0]
+    pr_port = str(proxy_address[1])
+    try:
+        data = socket.recv(1024).decode('utf-8')
+        print('Recibido -- ', data)
+    except:
+        mess = 'Error: No server listening at ' + pr_ip
+        mess += ' port ' + pr_port
+        sys.exit(mess)
+    return data
 
 def get_file():
     if os.path.exists(sys.argv[1]):
@@ -78,7 +114,16 @@ if __name__ == '__main__':
   with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect(proxy_address)
     if method == 'REGISTER':
-      pass
+        send_register(my_socket, option)
+        data = receive_message(my_socket)
+        if '401 Unauthorized' in data:
+            print('Recibido -- 401 Unauthorized')
+            nonce = data.split('\r\n')[1].split('"')[-2]
+            passwd = config['account_passwd']
+            username = config['account_username']
+            digest = digest_response(nonce, passwd, username)
+            send_register_digest(my_socket, int(option), digest)
+            data = receive_message(my_socket)
     elif method == 'INVITE':
       pass
     elif method == 'BYE':
