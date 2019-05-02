66a5d68fe4868a9bd0482348677b7449e90b670b
anabahin <alinabahin3@gmail.com>
2019-05-02 19:42:42 +0200

    Update proxy_registrar.py
    implementados mensajes REGISTER

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -31,6 +31,21 @@ def get_file():
     else:
         sys.exit('file', sys.argv[1], 'not found')
 
+def digest_nonce(proxy_name):
+    digest = md5()
+    digest.update(bytes(proxy_name, 'utf-8'))
+    digest.digest()
+    return digest.hexdigest()
+
+
+def digest_response(nonce, passwd, username):
+    digest = md5()
+    digest.update(bytes(nonce, 'utf-8'))
+    digest.update(bytes(passwd, 'utf-8'))
+    digest.update(bytes(username, 'utf-8'))
+    digest.digest()
+    return digest.hexdigest()
+
 
 class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
     dicc = {}
@@ -51,8 +66,46 @@ class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         self.json2registered()
         receive = self.rfile.read().decode('utf-8')
+        ip = self.client_address[0]
+        port = str(self.client_address[1])
+        method = receive.split()[0]
+        print('Recibido -- ', method)
         if method == 'REGISTER':
-            pass
+            user = receive.split('\r\n')[0].split()[1].split(':')[1]
+            if user in self.dicc:
+                expires = receive.split('\r\n')[1].split()[-1]
+                if int(expires) == 0:
+                    del self.dicc[user]
+                else:
+                    time_exp = gmtime(time() + 3600 + int(expires))
+                    date_format = '%Y-%m-%d %H:%M:%S'
+                    self.dicc[user][2] = strftime(date_format, time_exp)
+                self.wfile.write(b'SIP/2.0 200 OK\r\n')
+                print('Enviado -- 200 OK')
+            else:
+                if 'Digest response' in receive:
+                    nonce = digest_nonce(config['server_name'])
+                    response = digest_response(nonce, self.passwd[user], user)
+                    resp_user = receive.split('\r\n')[2].split('"')[-2]
+                    if response == resp_user:
+                        ip = self.client_address[0]
+                        port = receive.split('\r\n')[0].split()[1].split(':')[2]
+                        caddress = ip + ':' + port
+                        expires = receive.split('\r\n')[1].split()[-1]
+                        time_exp = gmtime(time() + 3600 + int(expires))
+                        exp = strftime('%Y-%m-%d %H:%M:%S', time_exp)
+                        self.dicc[user] = [ip, port, exp]
+                        self.wfile.write(b'SIP/2.0 200 OK\r\n')
+                        print('Enviado -- 200 OK')
+                    else:
+                        self.wfile.write(b'SIP/2.0 400 Bad Request\r\n')
+                        print('Enviado -- 400 Bad Request')
+                else:
+                    nonce = digest_nonce(config['server_name'])
+                    mess = 'SIP/2.0 401 Unauthorized\r\nWWW Authenticate: '
+                    mess += 'Digest nonce="' + nonce + '"\r\n'
+                    self.wfile.write(bytes(mess, 'utf-8'))
+                    print('Enviado -- 401 Unauthorized')
         elif method == 'INVITE':
             pass
         elif method == 'ACK':
