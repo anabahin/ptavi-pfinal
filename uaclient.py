32f1f88d47d03488b49109db463fa0850b15481f
anabahin <alinabahin3@gmail.com>
2019-05-24 11:46:00 +0200

    Update uaclient.py
    implementado envio de ack, escritura de fichero de log y reproduccion por mp32rtp y cvlc

--- uaclient.py
+++ uaclient.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import time
 import socket
 from hashlib import md5
 from xml.sax import make_parser
@@ -25,7 +26,7 @@ class XMLHandler(ContentHandler):
     def get_tags(self):
         return self.tags
 
-    
+
 def digest_response(nonce, passwd, username):
     digest = md5()
     digest.update(bytes(nonce, 'utf-8'))
@@ -39,44 +40,88 @@ def send_register(socket, option):
     mess = 'REGISTER sip:' + config['account_username'] + ':'
     mess += config['uaserver_puerto'] + ' SIP/2.0\r\nExpires: ' + str(option)
     print('Enviado -- ', mess)
+    log_mess = 'Sent to ' + proxy_address[0] + ':' + str(proxy_address[1])
+    log_mess += ': ' + mess.replace('\r\n', ' ')
+    write_log(log_mess)
     socket.send(bytes(mess, 'utf-8'))
-    
-    
+
+
 def send_register_digest(socket, option, digest):
     mess = 'REGISTER sip:' + config['account_username'] + ':'
     mess += config['uaserver_puerto'] + ' SIP/2.0\r\nExpires: ' + str(option)
     mess += '\r\nAuthorization: Digest response="'
     mess += digest + '"\r\n'
     print('Enviado -- ', mess)
+    log_mess = 'Sent to ' + proxy_address[0] + ':' + str(proxy_address[1])
+    log_mess += ': ' + mess.replace('\r\n', ' ')
+    write_log(log_mess)
     socket.send(bytes(mess, 'utf-8'))
 
-    
+
 def send_invite(socket, option):
     mess = 'INVITE sip:' + option + ' SIP/2.0\r\nContent-Type: '
     mess += 'application/sdp\r\n\r\nv=0\r\no=' + config['account_username']
     mess += ' ' + config['uaserver_ip'] + '\r\ns=misesion\r\nt=0\r\nm=audio '
     mess += config['rtpaudio_puerto'] + ' RTP\r\n'
     print('Enviado -- ', mess)
+    log_mess = 'Sent to ' + proxy_address[0] + ':' + str(proxy_address[1])
+    log_mess += ': ' + mess.replace('\r\n', ' ')
+    write_log(log_mess)
     socket.send(bytes(mess, 'utf-8'))
 
 
 def send_bye(socket, option):
     mess = 'BYE sip:' + option + ' SIP/2.0\r\n'
     print('Enviado -- ', mess)
+    log_mess = 'Sent to ' + proxy_address[0] + ':' + str(proxy_address[1])
+    log_mess += ': ' + mess.replace('\r\n', ' ')
+    write_log(log_mess)
     socket.send(bytes(mess, 'utf-8'))
-    
-    
+
+
+def send_ack(socket, option):
+    mess = 'ACK sip:' + option + ' SIP/2.0\r\n'
+    print('Enviado -- ', mess)
+    log_mess = 'Sent to ' + proxy_address[0] + ':' + str(proxy_address[1])
+    log_mess += ': ' + mess.replace('\r\n', ' ')
+    write_log(log_mess)
+    socket.send(bytes(mess, 'utf-8'))
+
+
+def send_message(socket, method, option):
+    mess = method.upper() + ' sip:' + option + ' SIP/2.0\r\n'
+    print('Enviado -- ', mess)
+    log_mess = 'Sent to ' + proxy_address[0] + ':' + str(proxy_address[1])
+    log_mess += ': ' + mess.replace('\r\n', ' ')
+    write_log(log_mess)
+    socket.send(bytes(mess, 'utf-8'))
+
+
 def receive_message(socket):
     pr_ip = proxy_address[0]
     pr_port = str(proxy_address[1])
     try:
         data = socket.recv(1024).decode('utf-8')
         print('Recibido -- ', data)
+        log_mess = 'Received from ' + pr_ip + ':' + pr_port
+        log_mess += ': ' + data.replace('\r\n', ' ')
+        write_log(log_mess)
     except:
-        mess = 'Error: No server listening at ' + pr_ip
-        mess += ' port ' + pr_port
-        sys.exit(mess)
+        log_mess = 'Error: No server listening at ' + pr_ip
+        log_mess += ' port ' + pr_port
+        write_log(log_mess)
+        sys.exit(log_mess)
     return data
+
+
+def date():
+    now = time.gmtime(time.time() + 3600)
+    return time.strftime('%Y%m%d%H%M%S', now)
+
+
+def write_log(line):
+    with open(config['log_path'], 'a') as log:
+        log.write(date() + ' ' + line + '\n')
 
 
 def mp32rtp(ip, port):
@@ -116,34 +161,32 @@ def get_arguments():
     method = get_method()
     option = get_option(method)
     return xml_file, method, option
-    
-    
-if __name__ == '__main__':
-  
-  conf = {'account': ['username', 'passwd'],
-          'uaserver': ['ip', 'puerto'],
-          'rtpaudio': ['puerto'],
-          'regproxy': ['ip', 'puerto'],
-          'log': ['path'],
-          'audio': ['path']}
-  
-  if len(sys.argv) == 4:
-      xml_file, method, option = get_arguments()
-  else:
-      sys.exit('usage: python3 uaclient.py <fichero xml> <SIP method> <option>')
 
-  parser = make_parser()
-  xmlhandler = XMLHandler(conf)
-  parser.setContentHandler(xmlhandler)
-  parser.parse(open(xml_file))
-  config = xmlhandler.get_tags()
+conf = {'account': ['username', 'passwd'],
+        'uaserver': ['ip', 'puerto'],
+        'rtpaudio': ['puerto'],
+        'regproxy': ['ip', 'puerto'],
+        'log': ['path'],
+        'audio': ['path']}
 
-  if not os.path.exists(config['log_path']):
-      os.system('touch ' + config['log_path'])
+if len(sys.argv) == 4:
+    xml_file, method, option = get_arguments()
+else:
+    sys.exit('usage: python3 uaclient.py <fichero xml> <SIP method> <option>')
 
-  proxy_address = (config['regproxy_ip'], int(config['regproxy_puerto']))
-  
-  with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+parser = make_parser()
+xmlhandler = XMLHandler(conf)
+parser.setContentHandler(xmlhandler)
+parser.parse(open(xml_file))
+config = xmlhandler.get_tags()
+
+if not os.path.exists(config['log_path']):
+    os.system('touch ' + config['log_path'])
+
+proxy_address = (config['regproxy_ip'], int(config['regproxy_puerto']))
+write_log('Starting client...')
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect(proxy_address)
     if method == 'REGISTER':
         send_register(my_socket, option)
@@ -175,5 +218,6 @@ if __name__ == '__main__':
     else:
         send_message(my_socket, method, option)
         data = receive_message(my_socket)
-    
-  print('Socket terminado')
+
+print("Socket terminado.")
+write_log('Finishing client.')
